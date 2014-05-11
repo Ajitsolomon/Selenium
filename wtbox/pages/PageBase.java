95d10d6c50664007c1d393a49970f058b52f53e1
Ajit Solomon <ajitsolomon@hotmail.com>
2014-05-11 14:32:25 -0700

    wt box files

---
+++ wtbox/pages/PageBase.java
@@ -0,0 +1,122 @@
+package wtbox.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+/**
+ * Page Object base class.  It provides the base structure 
+ * and properties for a page object to extend.  
+ * 
+ */
+public class PageBase {
+	  /** Default URL */
+	  protected String URL;	
+	  
+	  /** This page's WebDriver */ 
+	  protected WebDriver driver; 
+	  
+	  /** Expected Page Title.  This will be used in isPageLoad() 
+	   * to check if page is loaded. */
+	  protected String pageTitle; 
+	  
+	  
+	  /** Constructor */ 
+	  public PageBase(WebDriver driver, String pageTitle) {
+		  this.driver = driver; 
+		  this.pageTitle = pageTitle; 
+	  }
+	  
+	  /** 
+	   * Check if page is loaded by comparing 
+	   * the expected page-title with an actual page-title. 
+	   **/ 
+	  public boolean isPageLoad(){
+		  return (driver.getTitle().contains(pageTitle)); 
+	  }
+	  
+	  
+	  /** Open the default page */ 
+	  public void open(){
+		  driver.get(URL); 
+	  }
+	  
+	  
+	  /** Returns the page title */ 
+	  public String getTitle() {
+		  return pageTitle; 
+	  }
+	  
+	  /** Returns the default URL */ 
+	  public String getURL() {
+		return URL;
+	  }
+	  
+	  /** 
+	   * Send text keys to the element that finds by cssSelector.  
+	   * It shortens "driver.findElement(By.cssSelector()).sendKeys()". 
+	   * @param cssSelector
+	   * @param text
+	   */
+	  protected void sendText(String cssSelector, String text) {
+			driver.findElement(By.cssSelector(cssSelector)).sendKeys(text);
+	  }
+	  
+	  /** Is the text present in page. */ 
+	  public boolean isTextPresent(String text){
+		  return driver.getPageSource().contains(text); 
+	  }
+	  
+	  /** Is the Element in page. */
+	  public boolean isElementPresent(By by) {
+			try {
+				driver.findElement(by);//if it does not find the element throw NoSuchElementException, thus returns false. 
+				return true;
+			} catch (NoSuchElementException e) {
+				return false;
+			}
+	  }
+
+	  /** 
+	   * Is the Element present in the DOM. 
+	   * 
+	   * @param _cssSelector 		element locater
+	   * @return					WebElement
+	   */
+	  public boolean isElementPresent(String _cssSelector){
+			try {
+				driver.findElement(By.cssSelector(_cssSelector));
+				return true;
+			} catch (NoSuchElementException e) {
+				return false;
+			}
+	  }
+	  
+
+	  /**
+		* Checks if the elment is in the DOM and displayed. 
+		* 
+		* @param by - selector to find the element
+		* @return true or false
+		*/
+	  public boolean isElementPresentAndDisplay(By by) {
+			try {			
+				return driver.findElement(by).isDisplayed();
+			} catch (NoSuchElementException e) {
+				return false;
+			}
+	  }
+	  
+	  /** 
+	   * Returns the first WebElement using the given method.  	   
+	   * It shortens "driver.findElement(By)". 
+	   * @param by 		element locater. 
+	   * @return 		the first WebElement
+	   */
+	  public WebElement getWebElement(By by){
+		  	return driver.findElement(by); 			
+	  }
+}
